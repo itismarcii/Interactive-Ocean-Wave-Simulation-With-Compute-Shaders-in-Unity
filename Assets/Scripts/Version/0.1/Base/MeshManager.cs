406c21d776c52d6c6c6d432c53af4d3edb2ba9ad
itismarcii <58007324+itismarcii@users.noreply.github.com>
2023-02-03 17:27:21 +0100

    Namespace update

--- Assets/Scripts/Version/0.1/Base/MeshManager.cs
+++ Assets/Scripts/Version/0.1/Base/MeshManager.cs
@@ -1,68 +1,68 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
-using Version._0._1.Base;
 using Debug = UnityEngine.Debug;
 
-public class MeshManager : MonoBehaviour
+namespace Version._0._1.Base
 {
-    [SerializeField] private MeshFilter _MeshFilter;
-    [SerializeField] private float MaxHeightAmplifier = 0;
-    [SerializeField] private WaveInformation[] _Wave;
-    private int _MeshVertexCount = 0;
-    [Space, SerializeField] private int _AmountOfWaveCalculations = 1;
-
-    private readonly Stopwatch _Stopwatch = new Stopwatch();
-    private float _StartTime = 0;
-    private const float _END_TIME = 60;
-    private TimeSpan _DurationCalculations;
-    private int _CalculationAmount = 0;
-
-    private void Start()
+    public class MeshManager : MonoBehaviour
     {
-        GerstnerWave.SetBaseGrid(_MeshFilter.mesh.vertices);
-        _MeshVertexCount = _MeshFilter.mesh.vertexCount;
-    }
+        [SerializeField] private MeshFilter _MeshFilter;
+        [SerializeField] private float MaxHeightAmplifier = 0;
+        [SerializeField] private WaveInformation[] _Wave;
+        private int _MeshVertexCount = 0;
+        [Space, SerializeField] private int _AmountOfWaveCalculations = 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_StartTime >= _END_TIME)
+        private readonly Stopwatch _Stopwatch = new Stopwatch();
+        private float _StartTime = 0;
+        private const float _END_TIME = 60;
+        private TimeSpan _DurationCalculations;
+        private int _CalculationAmount = 0;
+
+        private void Start()
         {
-            var average = _DurationCalculations / _CalculationAmount;
-            Debug.Log($"Total: {_DurationCalculations}  Avg: {average}  AvgInMilli: {average.Milliseconds}");
-            return;
+            GerstnerWave.SetBaseGrid(_MeshFilter.mesh.vertices);
+            _MeshVertexCount = _MeshFilter.mesh.vertexCount;
         }
-        
 
-        GerstnerWave.UpdateTime(Time.deltaTime);
-        GerstnerWave.MaxHeightAmplifier = MaxHeightAmplifier;
-
-        _Stopwatch.Start();
-        TestEnvironment();
-        _Stopwatch.Stop();
-        _DurationCalculations += _Stopwatch.Elapsed;
-        _CalculationAmount++;
-        
-        _Stopwatch.Reset();
-
-        _StartTime += Time.deltaTime;
-    }
-
-    private void TestEnvironment()
-    {
-        var meshVertices = new Vector3[_MeshVertexCount];
-
-        for (var i = 0; i < _AmountOfWaveCalculations; i++)
+        // Update is called once per frame
+        void Update()
         {
-            foreach (var waveInformation in _Wave)
+            if (_StartTime >= _END_TIME)
             {
-                GerstnerWave.GerstnerWaveDisplacement(ref meshVertices, waveInformation);
+                var average = _DurationCalculations / _CalculationAmount;
+                Debug.Log($"Total: {_DurationCalculations}  Avg: {average}  AvgInMilli: {average.Milliseconds}");
+                return;
             }
+        
+
+            GerstnerWave.UpdateTime(Time.deltaTime);
+            GerstnerWave.MaxHeightAmplifier = MaxHeightAmplifier;
+
+            _Stopwatch.Start();
+            TestEnvironment();
+            _Stopwatch.Stop();
+            _DurationCalculations += _Stopwatch.Elapsed;
+            _CalculationAmount++;
+        
+            _Stopwatch.Reset();
+
+            _StartTime += Time.deltaTime;
         }
 
-        _MeshFilter.mesh = GerstnerWave.ApplyDisplacement(ref meshVertices, _MeshFilter.mesh);
+        private void TestEnvironment()
+        {
+            var meshVertices = new Vector3[_MeshVertexCount];
+
+            for (var i = 0; i < _AmountOfWaveCalculations; i++)
+            {
+                foreach (var waveInformation in _Wave)
+                {
+                    GerstnerWave.GerstnerWaveDisplacement(ref meshVertices, waveInformation);
+                }
+            }
+
+            _MeshFilter.mesh = GerstnerWave.ApplyDisplacement(ref meshVertices, _MeshFilter.mesh);
+        }
     }
 }
